c4ab5877e75ced89e283dca3cfc2c518878abfdf
echandranagarjun@gmail.com <echandranagarjun@gmail.com>
2022-02-10 12:01:33 +0530

    HelloWorldProgram

---
+++ JavaPrograms/src/com/ecn/helloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ecn.helloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welcome to the world");
+		
+	}
+
+}
